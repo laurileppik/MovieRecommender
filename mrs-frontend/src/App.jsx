1810be4ccfc65409ffca7918994f29f05d0ead88
lauri.leppik <laurile02@gmail.com>
2024-03-09 18:29:38 +0200

    merging front and backend

---
+++ mrs-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListMovieComponent from './components/ListMovieComponent'
+
+function App() {
+
+  return (
+    <>
+        <ListMovieComponent />
+    </>
+  )
+}
+
+export default App
---
+++ mrs-frontend/src/components/ListMovieComponent.jsx
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react'
+import { listMovies } from '../services/MovieService'
+
+const ListMovieComponent = () => {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        listMovies().then((response) => {
+            setMovies(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }, []) 
+
+  return (
+    <div className='container'>
+        <h2 className='text-center'>List of Movies</h2>
+        <table className='table table-striped table-bordered'>
+            <thead>
+                <tr>
+                    <th>Movie Id</th>
+                    <th>Movie Name</th>
+                    <th>Genre</th>
+                    <th>Language</th>
+                    <th>Start time</th>
+                    <th>End time</th>
+                    <th>Minimum age</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    movies.map(movie => 
+                        <tr key={movie.id}>
+                            <td>{movie.id}</td>
+                            <td>{movie.name}</td>
+                            <td>{movie.genre}</td>
+                            <td>{movie.language}</td>
+                            <td>{movie.startTime}</td>
+                            <td>{movie.endTime}</td>
+                            <td>{movie.minimumAge}</td>
+                        </tr>)
+                }
+                <tr>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListMovieComponent
